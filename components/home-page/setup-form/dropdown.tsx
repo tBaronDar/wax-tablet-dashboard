df412161a83298805e09bc48c7f6df52098b5575
tBaronDar <thedarelis@gmail.com>
2024-04-12 15:10:37 +0300

    finished dropdown lists

--- components/home-page/setup-form/dropdown.tsx
+++ components/home-page/setup-form/dropdown.tsx
@@ -2,25 +2,25 @@
 
 import { useState } from "react";
 
-function Dropdown({ children }) {
-  const [styles, setStyles] = useState({ display: "block" });
+function Dropdown({ children, selectedValue }) {
+	const [styles, setStyles] = useState({ display: "block" });
 
-  function clickHandler() {
-    if (styles.display === "none") {
-      setStyles({ display: "block" });
-    }
-    if (styles.display === "block") {
-      setStyles({ display: "none" });
-    }
-  }
-  const stylesObj = { display: "block" };
+	function clickHandler() {
+		if (styles.display === "none") {
+			setStyles({ display: "block" });
+		}
+		if (styles.display === "block") {
+			setStyles({ display: "none" });
+		}
+	}
+	const stylesObj = { display: "block" };
 
-  return (
-    <div>
-      <button onClick={clickHandler}>Drop list</button>
-      <div style={styles}>{children}</div>
-    </div>
-  );
+	return (
+		<div>
+			<button onClick={clickHandler}>{selectedValue}</button>
+			<div style={styles}>{children}</div>
+		</div>
+	);
 }
 
 export default Dropdown;
--- components/home-page/setup-form/form.tsx
+++ components/home-page/setup-form/form.tsx
@@ -1,48 +1,70 @@
 "use client";
 
+import { useState } from "react";
 import { getData } from "@/lib/get-formdata";
 import Dropdown from "./dropdown";
 
 function SetupForm({ username, password, collections, databases }) {
-  return (
-    <section>
-      <h1>Setup your database</h1>
-      <form action={getData}>
-        <div>
-          <div>
-            <label htmlFor="username">Username</label>
-            <input
-              id="username"
-              name="username"
-              type="text"
-              defaultValue={username}
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
-              id="password"
-              name="password"
-              type="text"
-              defaultValue={password}
-            />
-          </div>
-        </div>
-        <Dropdown>
-          {collections.map((name: string) => {
-            return <p key={name}>{name}</p>;
-          })}
-        </Dropdown>
-        <Dropdown>
-          {databases.map((name: string) => {
-            return <p key={name}>{name}</p>;
-          })}
-        </Dropdown>
-        <button type="submit">Send</button>
-        <button type="button">Change</button>
-      </form>
-    </section>
-  );
+	const [selectedCollection, setSelectedCollection] = useState("Select one");
+	const [selectedDatabase, setSelectedDatabase] = useState("Select one");
+
+	function collectionSelectHandler(name: string) {
+		setSelectedCollection(name);
+	}
+
+	function databaseSelectHandler(name: string) {
+		setSelectedDatabase(name);
+	}
+
+	return (
+		<section>
+			<h1>Setup your database</h1>
+			<form action={getData}>
+				<div>
+					<div>
+						<label htmlFor="username">Username</label>
+						<input
+							id="username"
+							name="username"
+							type="text"
+							defaultValue={username}
+						/>
+					</div>
+					<div>
+						<label htmlFor="password">Password</label>
+						<input
+							id="password"
+							name="password"
+							type="text"
+							defaultValue={password}
+						/>
+					</div>
+				</div>
+				//collection dropdown list
+				<Dropdown selectedValue={selectedCollection}>
+					{collections.map((name: string) => {
+						return (
+							<p key={name} onClick={collectionSelectHandler.bind(null, name)}>
+								{name}
+							</p>
+						);
+					})}
+				</Dropdown>
+				//database dropdown list
+				<Dropdown selectedValue={selectedDatabase}>
+					{databases.map((name: string) => {
+						return (
+							<p key={name} onClick={databaseSelectHandler.bind(null, name)}>
+								{name}
+							</p>
+						);
+					})}
+				</Dropdown>
+				<button type="submit">Send</button>
+				<button type="button">Change</button>
+			</form>
+		</section>
+	);
 }
 
 export default SetupForm;
