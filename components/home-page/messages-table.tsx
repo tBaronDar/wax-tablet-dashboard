6183c9528a1e53ad687e84274fc8fca0fd46bbda
tBaronDar <thedarelis@gmail.com>
2024-05-10 14:43:59 +0300

    delete icon added

--- components/home-page/messages-table.tsx
+++ components/home-page/messages-table.tsx
@@ -1,33 +1,36 @@
 "use client";
 
-import { mongoMessageEraser, mongoMessagesGetter } from "@/lib/mongoDB-handler";
+import Image from "next/image";
+import { mongoMessageEraser } from "@/lib/mongoDB-handler";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
+
+import svgTrash from "@/public/images/trash.svg";
 
 import classes from "./messages-table.module.css";
 
 function MessagesTable({ messagesIn }) {
-	const { data: session } = useSession();
-	const [messages, setMessages] = useState(messagesIn);
+  const { data: session } = useSession();
+  const [messages, setMessages] = useState(messagesIn);
 
-	useEffect(() => {
-		setMessages(messagesIn);
-	}, [messagesIn]);
+  useEffect(() => {
+    setMessages(messagesIn);
+  }, [messagesIn]);
 
-	async function deleteMessageHandler(selectedMessage: string) {
-		if (
-			window.confirm("This action cannot be undone. Do you want to delete?")
-		) {
-			await mongoMessageEraser(session.user.email, selectedMessage);
-		}
-	}
+  async function deleteMessageHandler(selectedMessage: string) {
+    if (
+      window.confirm("This action cannot be undone. Do you want to delete?")
+    ) {
+      await mongoMessageEraser(session.user.email, selectedMessage);
+    }
+  }
 
-	if (messages.length > 0) {
-		return (
-			<div className={classes.master}>
-				<h2>This are the messages</h2>
-				<table className={classes.table}>
-					{/* <thead className={classes["table-head"]}>
+  if (messages.length > 0) {
+    return (
+      <div className={classes.master}>
+        <h2>This are the messages</h2>
+        <table className={classes.table}>
+          {/* <thead className={classes["table-head"]}>
 						<tr className={classes["table-row"]}>
 							<th></th>
 							<th></th>
@@ -35,31 +38,33 @@ function MessagesTable({ messagesIn }) {
 							<th></th>
 							<th></th>
 						</tr> */}
-					{/* </thead> */}
-					<tbody>
-						{messages.map((message) => (
-							<tr key={message.id} className={classes["table-row"]}>
-								<td>{messages.indexOf(message) + 1}</td>
-								<td>{message.name}</td>
-								<td>{message.email}</td>
-								<td>{message.message}</td>
-								<td>
-									<button
-										onClick={deleteMessageHandler.bind(null, message.message)}>
-										delete
-									</button>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-			</div>
-		);
-	}
+          {/* </thead> */}
+          <tbody>
+            {messages.map((message) => (
+              <tr key={message.id} className={classes["table-row"]}>
+                <td>{messages.indexOf(message) + 1}</td>
+                <td>{message.name}</td>
+                <td>{message.email}</td>
+                <td>{message.message}</td>
+                <td>
+                  <button
+                    onClick={deleteMessageHandler.bind(null, message.message)}
+                    className={classes.delete}
+                  >
+                    {<Image src={svgTrash} alt="del" height={48} width={48} />}
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 
-	if (!messages || messages.legth === 0) {
-		return <h2>There are no messages to show</h2>;
-	}
+  if (!messages || messages.legth === 0) {
+    return <h2>There are no messages to show</h2>;
+  }
 }
 
 export default MessagesTable;
--- components/layout/main-navigation.tsx
+++ components/layout/main-navigation.tsx
@@ -5,34 +5,28 @@ import classes from "./main-navigation.module.css";
 import Image from "next/image";
 
 export function MainNavigation() {
-	return (
-		<header className={classes.header}>
-			<div className={classes.logo}>
-				<Link href={"/"}>
-					<Image
-						src={svgIcon}
-						color="red"
-						alt={"Logo"}
-						width={100}
-						height={100}
-					/>
-				</Link>
-			</div>
-			<nav className={classes.navigation}>
-				<ul>
-					<li>
-						<Link href={"/"}>Home</Link>
-					</li>
-					<li>
-						<Link href={"/setup"}>Setup</Link>
-					</li>
-					<li>
-						<Link href={"/about"}>About</Link>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	);
+  return (
+    <header className={classes.header}>
+      <Link href={"/"}>
+        <div className={classes.logo}>
+          <Image src={svgIcon} alt={"Logo"} width={100} height={100} />
+        </div>
+      </Link>
+      <nav className={classes.navigation}>
+        <ul>
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/setup"}>Setup</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default MainNavigation;
